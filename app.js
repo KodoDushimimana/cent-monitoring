deb5bd455b894aa001cfe1bac177cbdf3c0cf1b9
KodoDushimimana <72461740+KodoDushimimana@users.noreply.github.com>
2022-03-01 06:54:55 +0200

    This is the first step to add a monitoring plaform for us this is going to be hosted for testing purposed

---
+++ app.js
@@ -0,0 +1,153 @@
+const express = require('express')
+const res = require('express/lib/response')
+const fetch = require('node-fetch')
+const Airtable = require('airtable');
+var moment = require('moment')
+
+const app = express()
+app.use(express.static('public'))
+app.use(express.json())
+const PORT = 3000
+
+const ulk = '81ESSHEQSE2CQEMEGPFQU5M8E78K8A4Q'
+const mpp = 'RJB3FFGJC9ELOE4JKS3TCDSX5VAZ5P4N'
+const aga = 'THI9KBS00EHBMIB5YKDSGZDY0SY0YJ5X'
+
+const ulkID = 643155
+const mppID = 704668
+const agaID = 595753
+
+const ulk_url = `https://monitoringapi.solaredge.com/site/${ulkID}/overview?api_key=${ulk}`
+const mpp_url = `https://monitoringapi.solaredge.com/site/${mppID}/overview?api_key=${mpp}`
+const ag_url = `https://monitoringapi.solaredge.com/site/${agaID}/overview?api_key=${aga}`
+
+const ulk_enviro = ` https://monitoringapi.solaredge.com/site/${ulkID}/envBenefits?systemUnits=Imperial&api_key=${ulk}`
+const mpp_enviro = ` https://monitoringapi.solaredge.com/site/${mppID}/envBenefits?systemUnits=Imperial&api_key=${mpp}`
+const aga_enviro = ` https://monitoringapi.solaredge.com/site/${agaID}/envBenefits?systemUnits=Imperial&api_key=${aga}`
+
+
+let now = moment().format('LLLL')
+
+console.log(now)
+
+
+
+
+// Airtable information 
+
+const base = new Airtable({apiKey: 'keyRtSv4UAZ79Kpoy'}).base('appGWfv8R1VHPYYOA');
+
+app.get('/ulkapi', async(req, res) =>{
+
+    const fetchUlk = await fetch(ulk_url)
+    const data = await fetchUlk.json()
+    res.send(data)
+   
+
+    const lastUpdate = data.overview. lastUpdateTime
+    const energyToday = data.overview.lastDayData.energy
+
+    
+    base('Monitoring').create([
+        {
+          "fields": {
+              "Last Update": lastUpdate,
+              "Today's Energy [Wh]": energyToday
+
+          }
+        },
+        ], function(err, records) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        records.forEach(function (record) {
+          console.log(record.getId());
+        });
+      });
+
+      if(lastUpdate < now){
+        base('Monitoring').update([
+            {
+              "id": "recY2tnaXxGYpuz5y",
+              "fields": {
+                "Last Update": lastUpdate,
+                "Today's Energy [Wh]": energyToday
+              }
+            }
+          ], function(err, records) {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            records.forEach(function(record) {
+              console.log(record.get('Last Update'));
+            });
+          });
+    }else{
+        
+        console.log(`now is ${now}`)
+    }
+
+
+})
+
+app.get('/mppapi', async(req, res) =>{
+
+    const fetchMpp = await fetch(mpp_url)
+    const data = await fetchMpp.json()
+    res.send(data)
+
+})
+
+app.get('/agapi', async(req, res) =>{
+
+    const fetchAg = await fetch(ag_url)
+    const data = await fetchAg.json()
+    res.send(data)
+
+})
+
+
+app.post('/add', (req, res)=>{
+    console.log('I got a request')
+    console.log(req.body)
+    const data = req.body.name
+
+    res.json({
+
+        name:data,
+        age: 19
+
+    })
+
+    res.end()
+
+})
+
+// Environmental Impact
+
+app.get('/ulkenviro', async(req, res) =>{
+    const fetchEnviro = await fetch (ulk_enviro)
+    const enviroData = await fetchEnviro.json()
+    res.send(enviroData)
+
+})
+
+app.get('/mppenviro', async(req, res) =>{
+    const fetchEnviro = await fetch (mpp_enviro)
+    const enviroData = await fetchEnviro.json()
+    res.send(enviroData)
+
+})
+
+app.get('/agaenviro', async(req, res) =>{
+    const fetchEnviro = await fetch (aga_enviro)
+    const enviroData = await fetchEnviro.json()
+    res.send(enviroData)
+
+})
+
+
+
+app.listen(PORT, ()=>{console.log(`Server is up and runing on port ${PORT}`)})
---
+++ public/js/index.js
@@ -0,0 +1,88 @@
+const ulklastupdate = document.getElementById('last_updated_ulk')
+const ulkenergyToday = document.getElementById('energy_today_ulk')
+
+const mpplastupdate = document.getElementById('last_updated_mpp')
+const mppenergyToday = document.getElementById('energy_today_mpp')
+
+const aglastupdate = document.getElementById('last_updated_agahozo')
+const agenergyToday = document.getElementById('energy_today_agahozo')
+
+const ulkenviro = document.getElementById('ulkTree')
+const mppeaceenviro = document.getElementById('mppTree')
+const agaenviro = document.getElementById('agaTree')
+
+const ulkCarbon = document.getElementById('ulk_corbon')
+const mppCarbon = document.getElementById('mpp_corbon')
+const agaCarbon = document.getElementById('aga_corbon')
+
+const ulk_url = '/ulkapi'
+const mpp_url = '/mppapi'
+const ag_url = '/agapi'
+
+const ulkData  = async()=>{
+
+    const fetchUlk = await fetch(ulk_url)
+    const data = await fetchUlk.json()
+    ulklastupdate.textContent = data.overview.lastUpdateTime
+    ulkenergyToday.textContent = data.overview.lastDayData.energy
+
+}
+
+const mppData  = async()=>{
+
+    const fetchMpp = await fetch(mpp_url)
+    const data = await fetchMpp.json()
+    mpplastupdate.textContent = data.overview.lastUpdateTime
+    mppenergyToday.textContent = data.overview.lastDayData.energy
+
+}
+
+const agData  = async()=>{
+
+    const fetchAg = await fetch(ag_url)
+    const data = await fetchAg.json()
+    aglastupdate.textContent = data.overview.lastUpdateTime
+    agenergyToday.textContent = data.overview.lastDayData.energy
+
+}
+
+ulkData()
+mppData()
+agData()
+
+const enviro_ulk = '/ulkenviro'
+const enviro_mpp = '/mppenviro'
+const enviro_aga = '/agaenviro'
+
+const ulkenviroBen = async() =>{
+    const fetchUlkEnviro = await fetch (enviro_ulk)
+    const enviroData = await fetchUlkEnviro.json()
+
+    ulkenviro.textContent = enviroData.envBenefits.treesPlanted.toFixed(2)
+    ulkCarbon.textContent = enviroData.envBenefits.gasEmissionSaved.co2
+
+}
+
+const mppenviroBen = async() =>{
+    const fetchmppEnviro = await fetch (enviro_mpp)
+    const enviroData = await fetchmppEnviro.json()
+
+    mppeaceenviro.textContent = enviroData.envBenefits.treesPlanted.toFixed(2)
+    mppCarbon.textContent = enviroData.envBenefits.gasEmissionSaved.co2
+
+}
+
+const agaenviroBen = async() =>{
+    const fetchagaEnviro = await fetch (enviro_aga)
+    const enviroData = await fetchagaEnviro.json()
+
+    agaenviro.textContent = enviroData.envBenefits.treesPlanted.toFixed(2)
+    agaCarbon.textContent = enviroData.envBenefits.gasEmissionSaved.co2
+
+}
+
+
+ulkenviroBen()
+mppenviroBen()
+agaenviroBen()
+
